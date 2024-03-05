d6e942162ad9902dcdc916e218d35329f53cf4bf
Ricardo Angarita <ricardoangarita@outlook.com>
2024-03-05 12:27:46 -0500

    Update variable lenguaje to language

--- hola_lenguaje.py
+++ hola_lenguaje.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    lenguaje= os.getenv("LENGUAJE")
+    lenguaje= os.getenv("LANGUAGE")
     print(f"Hola {lenguaje} desde GitHub, aprendiendo triggers")
 
 if __name__ == "__main__":
