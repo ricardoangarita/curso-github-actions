8199a290c00039930465ba4cd8a8f423a3bfa4ba
Ricardo Angarita <rangarita@arrivelogistics.com>
2024-03-05 12:19:25 -0500

    Update message in hola_lenguaje.py

--- hola_lenguaje.py
+++ hola_lenguaje.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     lenguaje= os.getenv("LENGUAJE")
-    print(f"Hola {lenguaje} desde GitHub")
+    print(f"Hola {lenguaje} desde GitHub, aprendiendo triggers")
 
 if __name__ == "__main__":
     main()
